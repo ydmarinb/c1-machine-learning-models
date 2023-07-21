efc6f3ea02453b51e314450d25ba5c90b79ade9d
ydmarinb <danielmarinbe@gmail.com>
2023-07-21 10:19:56 -0500

    Nuevo modelo

---
+++ model_v4/predict.py
@@ -0,0 +1,12 @@
+import pickle
+
+import os
+import sys
+
+with open('../model_v4.pkl', 'rb') as f:
+        rf = pickle.load(f)
+
+def make_prediction(input_data:list):
+    new_variables = [input_data]
+    return int(rf.predict(new_variables)[0])
+
